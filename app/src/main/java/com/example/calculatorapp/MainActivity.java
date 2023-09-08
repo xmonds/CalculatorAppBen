a962baaa62c102ea2cc6f8d4f1d084244066c384
xmonds <93551082+xmonds@users.noreply.github.com>
2023-09-08 12:54:49 -0500

    adding in button functionality

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(View v)
+    {
+        TextView main = v.findViewById(R.id.mainText);
+        switch (v.getId()){
+            case(R.id.zeroButton):
+
+            break;
+            case(R.id.oneButton):
+
+                break;
+
+
+        }
     }
 }
